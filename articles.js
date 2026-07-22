b6c61c0346e1ad50d359939da1a2c096834870df
lyyuan03 <lyyuan03@gmail.com>
2026-07-22 11:16:48 +0800

    更新付費試閱文章資料快取

--- articles.js
+++ articles.js
@@ -1,5 +1,5 @@
 import { db } from "./firebase-config.js";
-import { staticArticles } from "./static-articles.js?v=20260722-paid-pilot-1";
+import { staticArticles } from "./static-articles.js?v=20260722-paid-pilot-2";
 import { collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const categoryLabels = {
