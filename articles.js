83949b5f1e0bd55b3d51ef6f6a221d5bf7174a3d
lyyuan03 <lyyuan03@gmail.com>
2026-07-27 19:19:06 +0800

    套用會員文章最新快取

--- articles.js
+++ articles.js
@@ -1,5 +1,5 @@
 import { db } from "./firebase-config.js";
-import { staticArticles } from "./static-articles.js?v=20260726-yuanshen-eleven-1";
+import { staticArticles } from "./static-articles.js?v=20260727-jitong-member-1";
 import { collection, doc, getDocs, query, runTransaction, serverTimestamp, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const categoryLabels = {
