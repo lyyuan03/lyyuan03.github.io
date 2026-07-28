80ae186dc1a31660ee3a5607ecc7623d6831d37e
lyyuan03 <lyyuan03@gmail.com>
2026-07-28 16:21:56 +0800

    更新恢復文章快取版本

--- articles.js
+++ articles.js
@@ -1,5 +1,5 @@
 import { auth, db, isAdminEmail } from "./firebase-config.js";
-import { staticArticles } from "./static-articles.js?v=20260728-japan-temple-faith-2";
+import { staticArticles } from "./static-articles.js?v=20260728-restore-articles-1";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { collection, doc, getDoc, getDocs, query, runTransaction, serverTimestamp, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
