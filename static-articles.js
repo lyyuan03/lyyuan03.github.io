f1da505a5a4fe699bc4767556b2d3ef2961174ac
lyyuan03 <lyyuan03@gmail.com>
2026-07-30 16:28:28 +0800

    將致富文章主圖改為正式書封

--- static-articles.js
+++ static-articles.js
@@ -5,7 +5,7 @@ const featuredWealthDisciplineArticle = {
   ...wealthDisciplineArticle,
   publishedAt: "2026-07-30T23:59:00.000Z",
   updatedAt: "2026-07-30T23:59:00.000Z",
-  coverImage: "assets/articles/wealth-discipline/hand-book-photo.svg?v=20260730-hand-book-1"
+  coverImage: "assets/articles/wealth-discipline/book-cover-photo.svg?v=20260730-book-cover-1"
 };
 
 export const staticArticles = [featuredWealthDisciplineArticle, ...baseArticles];
