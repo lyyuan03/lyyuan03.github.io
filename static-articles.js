8f50c55e7b04b83ed867357412163f5692aeb37a
lyyuan03 <lyyuan03@gmail.com>
2026-07-30 16:29:42 +0800

    更新致富文章內容快取

--- static-articles.js
+++ static-articles.js
@@ -1,5 +1,5 @@
 import { staticArticles as baseArticles } from "./static-articles-base.js?v=20260730-safe-base-1";
-import { wealthDisciplineArticle } from "./article-wealth-discipline.js?v=20260730-wealth-discipline-2";
+import { wealthDisciplineArticle } from "./article-wealth-discipline.js?v=20260730-wealth-discipline-3";
 
 const featuredWealthDisciplineArticle = {
   ...wealthDisciplineArticle,
