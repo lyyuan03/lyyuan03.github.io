39c219d7918c183bb387592c346d5c5c3fae6738
lyyuan03 <lyyuan03@gmail.com>
2026-07-31 11:19:09 +0800

    更新名人托夢文章快取版本

--- static-articles.js
+++ static-articles.js
@@ -1,6 +1,6 @@
 import { staticArticles as baseArticles } from "./static-articles-base.js?v=20260730-safe-base-1";
 import { wealthDisciplineArticle } from "./article-wealth-discipline.js?v=20260730-wealth-discipline-6";
-import { celebrityDreamSpiritArticle } from "./article-celebrity-dream-spirit.js?v=20260731-celebrity-dream-1";
+import { celebrityDreamSpiritArticle } from "./article-celebrity-dream-spirit.js?v=20260731-celebrity-dream-2";
 
 const featuredWealthDisciplineArticle = {
   ...wealthDisciplineArticle,
