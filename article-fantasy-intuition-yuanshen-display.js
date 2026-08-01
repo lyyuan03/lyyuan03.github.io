97aaedeca06cd965f916abbea4369fd491e1fe49
lyyuan03 <lyyuan03@gmail.com>
2026-08-01 11:30:13 +0800

    修正幻覺直覺文章縮圖裁切

---
+++ article-fantasy-intuition-yuanshen-display.js
@@ -0,0 +1,6 @@
+import { fantasyIntuitionYuanshenArticle as baseArticle } from "./article-fantasy-intuition-yuanshen.js?v=20260801-1";
+
+export const fantasyIntuitionYuanshenArticle = {
+  ...baseArticle,
+  coverImage: "assets/articles/thumbnails/yuanshen-reality.jpg?v=20260801-thumbnail-fix-1"
+};
