efce698ba03102de4c7574125bd727a4ea698841
lyyuan03 <lyyuan03@gmail.com>
2026-08-01 19:48:01 +0800

    暫停後台分類模組自動載入以恢復文章列表

--- firebase-config.js
+++ firebase-config.js
@@ -6,11 +6,7 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-
 if (/(^|\/)articles\.html$/i.test(location.pathname)) {
   import("./article-protection.js?v=20260723-member-watermark-1");
   import("./article-paid-badge.js?v=20260722-3");
-  import("./article-taxonomy-v2.js?v=20260801-taxonomy-2");
-}
-
-if (/(^|\/)admin\.html$/i.test(location.pathname)) {
-  import("./article-taxonomy-v2.js?v=20260801-taxonomy-2");
+  import("./article-taxonomy-v2.js?v=20260801-taxonomy-3");
 }
 
 export const firebaseConfig = {
