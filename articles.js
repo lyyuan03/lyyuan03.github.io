8dc930beb4d228199cafbe8cc8c0cca3cc70e26a
lyyuan03 <lyyuan03@gmail.com>
2026-08-02 08:31:31 +0800

    更新文選靜態文章快取版本

--- articles.js
+++ articles.js
@@ -1,5 +1,5 @@
 import { auth, db, isAdminEmail } from "./firebase-config.js";
-import { staticArticles } from "./static-articles.js?v=20260801-fantasy-full-1";
+import { staticArticles } from "./static-articles.js?v=20260802-you-can-not-fear-death-1";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { collection, doc, getDoc, getDocs, query, runTransaction, serverTimestamp, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
