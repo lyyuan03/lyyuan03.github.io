c3d8111483c271e36182a8b0767e2b48d2f9da8a
lyyuan03 <lyyuan03@gmail.com>
2026-08-02 11:20:31 +0800

    更新縮圖控制快取版本

--- firebase-config.js
+++ firebase-config.js
@@ -9,14 +9,14 @@ if (/(^|\/)articles\.html$/i.test(currentPath)) {
   import("./article-protection.js?v=20260723-member-watermark-1");
   import("./article-paid-badge.js?v=20260722-3");
   import("./article-taxonomy-v2.js?v=20260801-taxonomy-3");
-  window.setTimeout(() => import("./article-thumbnail-display.js?v=20260802-thumbnail-independent-2"), 0);
+  window.setTimeout(() => import("./article-thumbnail-display.js?v=20260802-thumbnail-no-black-border-1"), 0);
 }
 
 if (/(^|\/)admin\.html$/i.test(currentPath)) {
   import("./article-admin-event-static-fix.js?v=20260802-event-admin-fix-1");
   import("./seed-guanyin-vow-lamp-v2.js?v=20260802-seed-1");
   import("./article-guanyin-v2-images.js?v=20260802-images-2");
-  window.setTimeout(() => import("./article-thumbnail-admin.js?v=20260802-thumbnail-independent-2"), 0);
+  window.setTimeout(() => import("./article-thumbnail-admin.js?v=20260802-thumbnail-no-black-border-1"), 0);
 }
 
 export const firebaseConfig = {
