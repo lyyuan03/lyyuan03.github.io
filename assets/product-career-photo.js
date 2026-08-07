f34216b5ac010ffef459ce53ffee9acf8b7fc2e6
lyyuan03 <lyyuan03@gmail.com>
2026-08-07 16:10:30 +0800

    Use new career omamori product photo

--- assets/product-career-photo.js
+++ assets/product-career-photo.js
@@ -1,1 +1,12 @@
-window.LYYProductImages=window.LYYProductImages||{};window.LYYProductImages.career='assets/products/career-omamori-20260806.webp?v=20260807-user-1';
+window.LYYProductImages=window.LYYProductImages||{};
+window.LYYProductImages.career='assets/products/career-omamori-photo-20260807.webp?v=20260807-career-photo-1';
+if(!document.querySelector('script[data-career-fit-loader]')){
+  const s=document.createElement('script');
+  s.src='assets/spiritual-aesthetics-career-fit.js?v=20260807-career-photo-1';
+  s.async=false;
+  s.dataset.careerFitLoader='true';
+  document.head.appendChild(s);
+}
+if(window.LYYApplyProductPhoto){
+  window.LYYApplyProductPhoto('career',window.LYYProductImages.career);
+}
