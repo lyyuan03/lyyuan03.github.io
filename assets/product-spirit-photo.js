217a754fef0b34e7230964cfc884e2114c3dc39b
lyyuan03 <lyyuan03@gmail.com>
2026-08-07 15:38:58 +0800

    Load strict contain styling for spirit product image

--- assets/product-spirit-photo.js
+++ assets/product-spirit-photo.js
@@ -1,1 +1,9 @@
-window.LYYProductImages=window.LYYProductImages||{};window.LYYProductImages.spirit='assets/products/spirit-omamori-20260806.webp?v=20260807-user-1';
+window.LYYProductImages=window.LYYProductImages||{};
+window.LYYProductImages.spirit='assets/products/spirit-omamori-20260806.webp?v=20260807-fit-3';
+if(!document.querySelector('script[data-spirit-fit-loader]')){
+  const s=document.createElement('script');
+  s.src='assets/spiritual-aesthetics-spirit-fit.js?v=20260807-fit-3';
+  s.async=false;
+  s.dataset.spiritFitLoader='true';
+  document.head.appendChild(s);
+}
