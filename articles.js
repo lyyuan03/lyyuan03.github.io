c15d5820f427ec769693029bdfddf6d74b36aea6
lyyuan03 <lyyuan03@gmail.com>
2026-08-10 21:19:43 +0800

    Refresh article core cache

--- articles.js
+++ articles.js
@@ -1,3 +1,3 @@
-import "./articles-core-20260808.js?v=20260810-detail-repair-3";
+import "./articles-core-20260808.js?v=20260810-photoreal-2";
 import "./article-love-beyond-filial-piety-display-fix.js?v=20260810-original-photo-fix-1";
 import "./sponsor-checkout-v3.js?v=20260808-email-renewal-1";
