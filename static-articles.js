33b576078a9830ea4d1a88e1a2dec82323699bfa
lyyuan03 <lyyuan03@gmail.com>
2026-08-10 18:56:45 +0800

    將新書幕後文章加入文選

--- static-articles.js
+++ static-articles.js
@@ -7,6 +7,7 @@ import { youCanNotFearDeathArticle } from "./article-you-can-not-fear-death.js?v
 import { howToJudgeTrueLingxiuUnderstandingArticle } from "./article-how-to-judge-true-lingxiu-understanding-display.js?v=20260803-publish-1";
 import { loveBeyondFilialPietyArticle } from "./article-love-beyond-filial-piety-photo.js?v=20260810-original-photo-fix-1";
 import { yuanshenDestinyArchetypeArticle } from "./article-yuanshen-destiny-archetype.js?v=20260807-final-1";
+import { thisBookTookThirtyYearsArticle } from "./article-this-book-took-thirty-years.js?v=20260810-1";
 
 const featuredWealthDisciplineArticle = {
   ...wealthDisciplineArticle,
@@ -23,6 +24,7 @@ const categoryNames = {
 };
 
 const seriesById = {
+  "this-book-took-thirty-years": "靈修辨證",
   "yuanshen-destiny-archetype": "靈修辨證",
   "love-beyond-filial-piety-and-ancestor-worship": "家族與靈魂",
   "how-to-judge-true-lingxiu-understanding": "靈修辨證",
@@ -47,6 +49,7 @@ function normalizeArticle(article) {
 }
 
 export const staticArticles = [
+  thisBookTookThirtyYearsArticle,
   yuanshenDestinyArchetypeArticle,
   loveBeyondFilialPietyArticle,
   howToJudgeTrueLingxiuUnderstandingArticle,
