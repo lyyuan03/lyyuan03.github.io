4f119db8bfdc4f43c91a642fc2a12fb4e253a889
lyyuan03 <lyyuan03@gmail.com>
2026-08-10 21:53:05 +0800

    Add corrected article thumbnail loader

---
+++ articles-v4.js
@@ -0,0 +1,3 @@
+import "./articles-core-20260810-v4.js?v=20260810-thumbnail-cover-1";
+import "./article-love-beyond-filial-piety-display-fix.js?v=20260810-original-photo-fix-1";
+import "./sponsor-checkout-v3.js?v=20260808-email-renewal-1";
