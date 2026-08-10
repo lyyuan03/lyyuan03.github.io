fd945563cebf6e0378d5c965a7c690a24cb5bb80
lyyuan03 <lyyuan03@gmail.com>
2026-08-10 14:58:00 +0800

    fix: restore articles.js to working core while image fix propagates

--- articles.js
+++ articles.js
@@ -1,2 +1,2 @@
-import "./articles-core-20260810.js?v=20260810-svg-restore-3";
+import "./articles-core-20260808.js?v=20260808-email-renewal-1";
 import "./sponsor-checkout-v3.js?v=20260808-email-renewal-1";
