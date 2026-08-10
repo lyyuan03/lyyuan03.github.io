9e7d06e498581ed13bdc57d3d54403e1aa1df60b
lyyuan03 <lyyuan03@gmail.com>
2026-08-10 19:58:26 +0800

    更新文選核心程式快取版本

--- articles.js
+++ articles.js
@@ -1,3 +1,3 @@
-import "./articles-core-20260808.js?v=20260810-new-book-article-2";
+import "./articles-core-20260808.js?v=20260810-core-repair-1";
 import "./article-love-beyond-filial-piety-display-fix.js?v=20260810-original-photo-fix-1";
 import "./sponsor-checkout-v3.js?v=20260808-email-renewal-1";
