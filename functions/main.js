68818fd53c6600ab84c531e65a605c718fa275a7
lyyuan03 <lyyuan03@gmail.com>
2026-08-26 14:12:37 +0800

    Load sponsor access normalizer

--- functions/main.js
+++ functions/main.js
@@ -6,6 +6,7 @@ const publicSponsorCheckoutFunctions = require("./public-sponsor-checkout-functi
 const publicSponsorCheckoutV2Functions = require("./public-sponsor-checkout-v2-functions");
 const memberOfferFunctions = require("./member-offers-functions");
 const articleNotifyFunctions = require("./article-notify-functions");
+const sponsorAccessNormalizer = require("./sponsor-access-normalizer");
 
 module.exports = {
   ...membershipFunctions,
@@ -13,6 +14,7 @@ module.exports = {
   ...publicSponsorCheckoutFunctions,
   ...memberOfferFunctions,
   ...articleNotifyFunctions,
+  ...sponsorAccessNormalizer,
   // 最後覆寫舊的公開贊助付款函式：改走綠界自動 callback 開通，並回報自動開通狀態。
   ...publicSponsorCheckoutV2Functions
 };
