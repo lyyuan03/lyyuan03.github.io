bc1294655378e799eebedc5fea8c638a8829d14c
lyyuan03 <lyyuan03@gmail.com>
2026-08-28 23:10:37 +0800

    更新草稿預覽快取以載入新文選

--- article-admin-draft-preview.js
+++ article-admin-draft-preview.js
@@ -1,5 +1,5 @@
 import { auth, db, isAdminEmail } from "./firebase-config.js";
-import { staticArticles } from "./static-articles.js?v=20260828-yuanqin-clean-text-2";
+import { staticArticles } from "./static-articles.js?v=20260828-yuanshen-old-manuscript-draft-1";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { collection, doc, getDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
