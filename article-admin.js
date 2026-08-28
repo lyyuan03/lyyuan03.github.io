9593a70bafa1d1df6d4b433f4eba7b455d669837
lyyuan03 <lyyuan03@gmail.com>
2026-08-28 15:34:45 +0800

    更新文章後台載入冤親債主新版同步程式

--- article-admin.js
+++ article-admin.js
@@ -1,4 +1,4 @@
-import "./article-admin-core.js?v=20260828-yuanqin-six-images-sync-1";
+import "./article-admin-core.js?v=20260828-yuanqin-final-text-sync-2";
 import "./article-admin-paid-security.js?v=20260825-paid-save-race-1";
 import { db } from "./firebase-config.js";
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
