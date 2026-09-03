96c9e802368f88f99c0eabd7c6ca02018afa0062
lyyuan03 <lyyuan03@gmail.com>
2026-09-03 18:17:31 +0800

    同步《靈性善終》後台圖片與前台 final PNG
    將 Firestore 舊圖路徑一次性遷移為四張 final PNG，後台內文圖片預覽與 inlineImageSettings 同步使用相同圖檔，縮圖預覽固定使用站內書封絕對網址，刷新 admin 快取；其他文章不動。

--- article-thumbnail-admin.js
+++ article-thumbnail-admin.js
@@ -26,7 +26,7 @@ const THUMBNAIL_SETTING_KEYS = [
 
 const FORCED_THUMBNAIL_IMAGES = {
   "yuanqin-debt-heart": "assets/articles/yuanqin-debt-heart/01-cover-yuanqin.webp?v=20260828-6",
-  "spiritual-good-death-last-visit": "assets/articles/spiritual-good-death/book-cover-thumb.jpg?v=20260903-admin-cover-1"
+  "spiritual-good-death-last-visit": "https://lyyuan.tw/assets/articles/spiritual-good-death/book-cover-thumb.jpg?v=20260903-admin-media-sync-1"
 };
 
 const RECOVERY_SETTINGS = {
