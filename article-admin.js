67136c885f7336e685bc0491a503e2315843b928
lyyuan03 <lyyuan03@gmail.com>
2026-09-18 16:39:50 +0800

    更新無形師與有形老師文選正文及圖檔
    同步更新安全匯入流程、封面、5 張正文圖與 9:16 無字縮圖。

--- article-admin.js
+++ article-admin.js
@@ -1,6 +1,6 @@
 import "./article-admin-core.js?v=20260916-clean-flow-5";
 import "./article-admin-paid-security.js?v=20260829-no-auto-overwrite-1";
-import "./article-admin-secure-import.js?v=20260828-yuanqin-secure-import-1";
+import "./article-admin-secure-import.js?v=20260918-need-teacher-refresh-1";
 import { db } from "./firebase-config.js?v=20260901-manual-only-1";
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
