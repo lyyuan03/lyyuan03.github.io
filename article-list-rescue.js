4dcbbfead7ed1deaff27f02288f9ab8ef1637103
lyyuan03 <lyyuan03@gmail.com>
2026-09-18 17:17:17 +0800

    同步 article-list-rescue.js 縮圖快取版本

--- article-list-rescue.js
+++ article-list-rescue.js
@@ -1,5 +1,5 @@
 import { staticArticles } from "./static-articles.js?v=20260902-detail-reading-rescue-2";
-import { resolveArticleThumbnailUrl } from "./article-thumbnail-url.js?v=20260916-clean-flow-5";
+import { resolveArticleThumbnailUrl } from "./article-thumbnail-url.js?v=20260918-photo-1";
 
 const root = document.getElementById("article-root");
 const params = new URLSearchParams(location.search);
